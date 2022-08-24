cb3c67d3523eef00d5e90a9eaa5bc1d0351227c9
mauridevcde <mauridevcde@gmail.com>
2022-08-24 16:36:47 -0400

    useEffect react 2

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,16 +1,34 @@
-import { useEffect } from "react";
+import { useState,useEffect } from "react";
 import { getGifs } from "../helpers/api_getGifs";
 
 export const GifGrid = ({ category }) => {
   
-    useEffect(() => {
-    getGifs(category);
+   const [images, setImages] = useState([]) 
+
+    const getImages = async () => {
+        const newImages = await getGifs(category);
+        setImages(newImages);
+    }
+
+     // el use efect se ejecuta cuando el componente es renderizado por primera vez, haciendo que no se vuelva a ejecutar esa porcion de codigo en proximos renderizados.
+    // si se le pasa un arreglo vacio como segundo parametro, se ejecutara solo una vez, cuando el componente es renderizado por primera vez
+  useEffect(() => {
+    getImages();
   }, []);
 
   return (
     <div>
       <h3>{category}</h3>
-      <p>Probando</p>
+
+        <ol>
+            {
+                images.map( ({id, title}) => (
+                    <li key = {id}>
+                        {title}
+                    </li>
+                ))
+            }
+        </ol>
     </div>
   );
 };
